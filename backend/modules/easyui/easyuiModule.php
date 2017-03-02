97e85719a220dfa023c6ce6bc92f151e99e27b83
Xemo Tangch <jmphper@163.com>
2017-03-02 15:12:50 +0800

    phpstorm提交

---
+++ backend/modules/easyui/easyuiModule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\easyui;
+
+/**
+ * easyui module definition class
+ */
+class easyuiModule extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\easyui\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
